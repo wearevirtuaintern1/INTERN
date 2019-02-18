d7cef29277915d42690f1a30cb7a436256608997
Patrycja <intern1@wearevirtua.com>
2019-02-18 11:39:08 +0100

    no message

--- src/Entity/ProductCategory.php
+++ src/Entity/ProductCategory.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -36,6 +38,16 @@ class ProductCategory
      */
     private $date_of_last_modification;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Product", mappedBy="category")
+     */
+    private $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -65,19 +77,19 @@ class ProductCategory
         return $this;
     }
 
-    public function getDateOfCreation(): \DateTimeInterface
+    public function getDateOfCreation()
     {
         return $this->date_of_creation;
     }
 
-    public function setDateOfCreation( $date_of_creation): self
+    public function setDateOfCreation($date_of_creation): self
     {
         $this->date_of_creation = $date_of_creation;
 
         return $this;
     }
 
-    public function getDateOfLastModification(): \DateTimeInterface
+    public function getDateOfLastModification()
     {
         return $this->date_of_last_modification;
     }
@@ -87,5 +99,46 @@ class ProductCategory
         $this->date_of_last_modification = $date_of_last_modification;
 
         return $this;
+    }
+
+    /**
+     * @return Collection|Product[]
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): self
+    {
+        if (!$this->products->contains($product)) {
+            $this->products[] = $product;
+            $product->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): self
+    {
+        if ($this->products->contains($product)) {
+            $this->products->removeElement($product);
+            // set the owning side to null (unless already changed)
+            if ($product->getCategory() === $this) {
+                $product->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function hasProduct(): bool
+    {
+        if ($this->products == null || $this->products->isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
+
     }
 }
